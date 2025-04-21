e65abeb11f213dc31eab6660dd8b167b7df0979f
Simon Puccio <simon.puccio@epitech.eu>
2025-04-21 23:54:45 +0200

    feat: integrate API for fetching exchange rates and update dependencies

--- lib/services/currency_api.dart
+++ lib/services/currency_api.dart
@@ -1,2 +1,20 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import '../secrets.dart';
+
+Future<Map<String, dynamic>> fetchExchangeRates() async {
+  final response = await http.get(
+    Uri.parse('https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?base=EUR'),
+    headers: {
+      'X-RapidAPI-Key': apiKey,
+      'X-RapidAPI-Host': 'currency-conversion-and-exchange-rates.p.rapidapi.com'
+    },
+  );
+  
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Failed to load exchange rates');
+  }
+}
